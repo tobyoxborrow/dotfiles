03c0d353dea74f53ac3cbfae2162771a17876807
Toby Oxborrow <toby@oxborrow.net>
2018-04-23 13:47:09 +0800

    Use gnu readlink/ln in dotfiles script
    The readlink and ln binaries in macOS do not support some of the options
    I want to use. Try to find the gnu equivilent or halt.

--- dotfiles.sh
+++ dotfiles.sh
@@ -39,6 +39,42 @@ dotfiles=(
 
 mkdir -p "$backup_dir"
 
+# Try to find GNU readlink from homebrew's coreutils package for macOS
+# The readlink in macOS doesn't support some of the options I want
+READLINK=""
+if [[ -f /usr/local/opt/coreutils/libexec/gnubin/readlink ]]; then
+    READLINK="/usr/local/opt/coreutils/libexec/gnubin/readlink"
+elif which greadlink; then
+    READLINK=$(which greadlink)
+else
+    READLINK=$(which readlink)
+fi
+# Is this GNU readlink?
+if ! $READLINK --version 2>&1 | grep -q GNU; then
+    echo "readlink is $READLINK"
+    echo "GNU readlink not found"
+    echo "On macOS install the coreutils package from homebrew"
+    exit 1
+fi
+
+# Try to find GNU ln from homebrew's coreutils package for macOS
+# The ln in macOS doesn't support some of the options I want
+LN=""
+if [[ -f /usr/local/opt/coreutils/libexec/gnubin/ln ]]; then
+    LN="/usr/local/opt/coreutils/libexec/gnubin/ln"
+elif which gln; then
+    LN=$(which gln)
+else
+    LN=$(which ln)
+fi
+# Is this GNU ln?
+if ! $LN --version 2>&1 | grep -q GNU; then
+    echo "ln is $LN"
+    echo "GNU ln not found"
+    echo "On macOS install the coreutils package from homebrew"
+    exit 1
+fi
+
 for dotfile in "${dotfiles[@]}"; do
     home_path="${HOME}/${dotfile}"
     repo_path="${dotfiles_dir}/${dotfile}"
@@ -47,16 +83,16 @@ for dotfile in "${dotfiles[@]}"; do
         echo "${dotfile}: not found in .dotfiles, skipping"
     elif [[ ! -f $home_path ]]; then
         echo "${dotfile}: not found, creating symlink"
-        ln -s "$repo_path" "$home_path"
+        $LN -s "$repo_path" "$home_path"
     elif [[ -h $home_path ]]; then
-        if [[ "$(readlink -f "$home_path")" != "$repo_path" ]]; then
+        if [[ "$($READLINK -f "$home_path")" != "$repo_path" ]]; then
             echo "${dotfile}: wrong symlink, overwriting"
-            ln -sfT "$repo_path" "$home_path"
+            $LN -sfT "$repo_path" "$home_path"
         fi
     elif [[ -f $home_path ]]; then
         echo "${dotfile}: regular file, replacing with symlink"
         mv "$home_path" "$backup_dir"
-        ln -s "$repo_path" "$home_path"
+        $LN -s "$repo_path" "$home_path"
     else
         echo "${dotfile}: unknown, skipping"
     fi
